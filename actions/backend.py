f3f691ae6461ff69f53c769b0b1e4c2013475402
UlvFoerlev <ulvfoerlev@gmail.com>
2024-08-12 15:18:24 +0200

    Updates

--- actions/backend.py
+++ actions/backend.py
@@ -1,10 +1,26 @@
 from streamcontroller_plugin_tools import BackendBase
-# from pydub import play, AudioSegment
+from pydub import play, AudioSegment
+from pathlib import Path
 
 
 class Backend(BackendBase):
-    def play_sound(self):
-        print("SAFDJAFKJFKLAJFSÆAJFSA")
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+        self.cached_sounds: dict[str, AudioSegment] = {}
+
+    def cache_sound(self, path: str | Path):
+        key = path if isinstance(path, str) else str(path)
+
+        self.cached_sounds[key] = AudioSegment.from_wav(path)
+
+    def play_sound(self, path: str | Path):
+        key = path if isinstance(path, str) else str(path)
+
+        if key not in self.cached_sounds:
+            self.cache_sound(path=path)
+
+        play(self.cached_sounds[key])
 
 
 backend = Backend()
