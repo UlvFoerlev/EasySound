347f2f8c92aa6c078455d068eeaebf14b2f321cc
UlvFoerlev <ulvfoerlev@gmail.com>
2024-08-17 14:17:09 +0200

    Fix

--- actions/play_sound/play_sound.py
+++ actions/play_sound/play_sound.py
@@ -20,7 +20,7 @@ class PlaySoundAction(SoundActionBase):
     @filepath.setter
     def filepath(self, value: str):
         if not isinstance(value, str):
-            value = ""
+            return
 
         self._set_property(key="filepath", value=value)
         self.filepath_input.set_text(self.filepath)
