d3321c6fe9472f5ad57d06cf921fd85f5cad661f
UlvFoerlev <ulvfoerlev@gmail.com>
2024-09-08 10:35:15 +0200

    On failure, set no filepath

--- actions/chooser.py
+++ actions/chooser.py
@@ -8,7 +8,7 @@ from typing import Any
 
 # Import globals
 import globals as gl
-from gi.repository import Gio, GLib, GObject, Gtk
+from gi.repository import Gio, GLib, GObject, Gtk, GError
 
 
 class ChooseFileDialog(Gtk.FileDialog):
@@ -30,7 +30,10 @@ class ChooseFileDialog(Gtk.FileDialog):
         self.open(callback=self.callback)
 
     def callback(self, dialog, result):
-        selected_file = self.open_finish(result)
+        try:
+            selected_file = self.open_finish(result)
+        except GError:
+            return
 
         self.selected_file = Path(selected_file.get_path())
         if self.setter_func:
